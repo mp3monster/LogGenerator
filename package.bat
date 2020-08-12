5054015e24f534171acd8541a618c448cc890897
Phil Wilkins <phil@mp3monster.org>
2020-08-12 20:53:33 +0100

    script to build jar file
    tidy up and run the steps to generate the jar file

---
+++ package.bat
@@ -0,0 +1,7 @@
+del ./classes/*
+del logGenerator.jar
+del ./jar/*.jar
+groovyc -d classes LogSimulator.groovy
+jar cfe ./jar/logSimulator.jar LogSimulator -C ./classes .
+
+REM now run with java -- java -jar logGenerator.jar
