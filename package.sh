60864a2678c3cce6917fb998e8a17e5cc90a0614
Phil Wilkins <phil@mp3monster.org>
2021-04-03 14:59:48 +0100

    convert to a JAR file

---
+++ package.sh
@@ -0,0 +1,7 @@
+rm -rf ./classes/*
+rm logGenerator.jar
+rm -rf ./jar/*.jar
+groovyc -d classes LogSimulator.groovy
+jar cfe ./jar/logSimulator.jar LogSimulator -C ./classes .
+
+# now run with java -- java -jar logGenerator.jar
