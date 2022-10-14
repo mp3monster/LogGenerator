5526bd47b297abc432f4b241c1bab53afd25c221
Phil Wilkins <phil@mp3monster.org>
2022-10-14 20:26:58 +0100

    Documentation and code for the vanilla solution for the custom outputter

--- CustomConsoleOutputter.groovy
+++ CustomConsoleOutputter.groovy
@@ -1,11 +1,22 @@
+ /*
+  * this is the most vanilla implementation of a Custom Outputter
+ */
  class CustomConsoleOutputter implements LogGenerator.RecordLogEvent
  {
   public CustomConsoleOutputter(){}
 
-    public initialize (Properties props, boolean verbose)    { System.out.println ("initializing ....")}
+    public initialize (Properties props, boolean verbose)    
+    { 
+      System.out.println ("initializing custom console outputter ....")
+    }
 
     public writeLogEntry(String entry)
     {
-        System.out.println ("Custom loaded logger - " + entry);
+        System.out.println ("Custom console logger event - " + entry);
+    }
+
+    public clearDown ()
+    {
+      System.out.println ("...clearing down custom console outputter")
     }
  }
---
+++ docs/CustomConsoleOutputter.md
@@ -0,0 +1,16 @@
+# Custom Console Outputter
+
+This is a very simple implementation to provide a skeleton for writing a Custom Outputter for the Log Simulator framework
+
+## Additional Configuration Attributes
+
+
+
+| Output Type configuration value | Description |
+| ------------------------------- | ----------- |
+|                                 |             |
+
+
+
+
+
